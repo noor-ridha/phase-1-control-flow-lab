e20985527003a90ab93df5d567a22a748e2e46ff
noor <noorridha028@gmail.com>
2022-10-25 14:17:20 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let result;
+  if (ride<= 400) {
+    result="This one is on me!"
+    return result;
+  } else if(ride>400 && ride<2000) {
+    result="That will be twenty bucks."
+    return result;
+  } else if (ride>2000 && ride<2500) {
+    result="I will gladly take your thirty bucks."
+    return result;
+  } else {
+    result="No can do."
+    return result;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+
+return ( city === "NYC" ? 'Ok, sounds good.': 'No go.');
+
+}
+ternaryCheckCity(city);
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+      break;
+      case "not as generous":
+        return "Thank you.";
+        break;
+        default:
+          return "Bye."
+  }
 }
